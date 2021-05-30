52361258cab49e94c5987e428063096b87600eb8
German Aquila <g.axl.aquila@gmail.com>
2021-05-29 22:11:03 -0300

    Add links to anchor tags

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import twitterlogo from "../scss/twitter.svg";
+import facebooklogo from "../scss/facebook.svg";
 
 const Footer = () => {
   return (
     <div id="white">
       <div id="footer">
         <a href="https://twitter.com/askwonderbot" target="_blank">
-          <i class="fa fa-twitter" aria-hidden="true"></i>
+          <img className="socialmedia-logo" src={twitterlogo} alt="" />
         </a>
          • 
         <a href="https://www.facebook.com/askwonderbot" target="_blank">
-          <i class="fa fa-facebook-official" aria-hidden="true"></i>
+          <img className="socialmedia-logo facebook" src={facebooklogo} alt="" />
         </a>{" "}
         • <a href="privacy.html">Privacy</a> • 
         <a href="cdn-cgi/l/email-protection.html#dbb1b4a9bfbab59bacb4b5bfbea9f6b9b4aff5b8b4b6">
