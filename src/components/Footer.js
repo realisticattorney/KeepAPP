6d85bc255345e8409e64d555f47ca1138f496c2d
German Aquila <g.axl.aquila@gmail.com>
2021-05-30 14:43:40 -0300

    Position call to action button to phone screen sizes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,14 +9,14 @@ const Footer = () => {
         <a href="https://twitter.com/at_3_a_m" target="_blank">
           <img className="socialmedia-logo" src={twitterlogo} alt="" />
         </a>
-         • 
+        <span className="dot-gone">•</span> 
         <a href="https://www.facebook.com/germanaquila" target="_blank">
           <img
             className="socialmedia-logo facebook"
             src={facebooklogo}
             alt=""
           />
-        </a>{" "}
+        </a>{" "} <br className="br-footer"/>
         • <a href="privacy.html">Privacy</a> • 
         <a href="mailto:g.axl.aquila@gmail.com">Contact</a> • 
         <a href="mailto:g.axl.aquila@gmail.com" target="_blank">
